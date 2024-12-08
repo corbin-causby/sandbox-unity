bb65517b457758fdc7054c8dddda1c762a0685ae
corbin-causby <corbincausby01@gmail.com>
2024-12-08 17:24:01 -0500

    Add project

---
+++ Sandbox/Assets/Scripts/Interactable.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;
+    public string interactionMessage = "Press E to Interact";
+    public bool hasEnteredRange = false;
+    
+    public Transform player;
+    
+    public Interactable currentInteractable = null;
+    bool hasInteracted = false;
+    bool hasPressedInteractKey = false;
+
+    public KeyCode interactKey = KeyCode.E;
+
+    void Update()
+    {
+        CheckForInteractable(); // if nothing is collided or in range then interactables are set to null until something is cxo  
+
+        if (currentInteractable != null)
+        {
+            // hasPressedInteractKey = false;
+            if (hasInteracted == false)
+            {
+                Debug.Log(interactionMessage);  
+
+                hasInteracted = true;
+                
+
+            }
+
+            if (hasInteracted == true && hasPressedInteractKey == false)
+            {
+                if (Input.GetKeyDown(interactKey))
+                {
+                    Interact();
+
+                    hasPressedInteractKey = true;
+                }
+            } 
+        }
+    }
+
+
+
+    // This function checks for nearby interactable objects
+    void CheckForInteractable()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
+        
+            foreach (Collider col in hitColliders)
+            {
+                // creates interactable when player collides with an Interactable which might be a npc or chest
+                Interactable interactable = col.GetComponent<Interactable>();
+
+                // If no collision with interactable
+                if (interactable != null)
+                {   
+                    // Check if the player(transform) is within range of interactable object and has not interacted with anything
+                    if (interactable.IsPlayerInRange(player.transform) )
+                    {
+                        currentInteractable = interactable;
+                        
+                        return; // Exit the function loop after finding the first interactable object
+                    }
+                }
+            }      
+
+        // resets params to null
+        hasInteracted = false;
+        hasPressedInteractKey = false;   
+        currentInteractable = null;
+    }
+    
+    public virtual void Interact ()
+    {   
+        Debug.Log("Interacting with " + gameObject.name);  
+    }
+
+    public bool IsPlayerInRange(Transform playerTransform)
+    {
+        float distance = Vector3.Distance(playerTransform.position, transform.position);
+        
+        return distance <= radius;
+
+        // function checks if player.transforms distance than the transform of the objects
+    }
+
+    void OnDrawGizmosSelected() 
+    {
+        Gizmos.color = Color.yellow;
+
+        // this unity function allows you to place a wire sphere around an object using the position of the transform and any radius 
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
---
+++ Sandbox/Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    // Think of character controller as motor that runs our character,
+    // and we have to tell the motor where to go.
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6f; // Public variables in the class will show in Unity UI
+    public float gravity = -9.81f; 
+    public float jumpHeight = 3f;   
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        // We want to move our character on the x andf z axis but not the y but all 3d components are a vector3
+
+        float horizontal = Input.GetAxisRaw("Horizontal"); // Horizontal stands for A and D keys
+        float vertical = Input.GetAxisRaw("Vertical"); // Vertical stands for the W and S keys
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized; // Vector3 class takes the X, Y, and Z as an argument
+        // or you could add the x by doing direction.x = Input.GetAxisRaw("Horizontal") 
+        // .normalized makes it so that when you are pressing two keys you are not going faster than the dedicated speed
+
+        if(direction.magnitude >= 0.1f) // .magnitude is length of direction vector; checking for any of the keys being pressed
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; // points player object in the direction the player is moving 
+            // Atan takes point the character is already looking and then the direction it ends and creates angle.
+            // This angle is then used below for the rotation of the CharacterController controllers transform being our player object
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime); // Look up documentation for Mathf.SmoothDampAngle, idk what this really does besides smoothing out the angle in unity
+            // This below creates rotation to the direction that the model is going in,
+            // first creating the target angle then smoothing it out and using that smoothed angle
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward; // dont really know what this does but it makes it so the character move direction is a combonation of the cameras direction and the characters direction inputted
+            controller.Move(moveDir.normalized * speed * Time.deltaTime); //Move funtion selects a given direction, once a direction key is inputted, 
+            // then multiplies it by speed which adds a force to the character
+            // Time.deltaTime makes it framerate independent...whatever that means
+
+        }
+
+        // Applying jump
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        
+        // Applying gravity below
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
