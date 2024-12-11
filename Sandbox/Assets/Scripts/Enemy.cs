b508c7b00b248ac2afa00060e8536b656b318772
corbin-causby <corbincausby01@gmail.com>
2024-12-11 00:25:59 -0500

    Interactable enemy

--- Sandbox/Assets/Scripts/Enemy.cs
+++ Sandbox/Assets/Scripts/Enemy.cs
@@ -1,95 +1,15 @@
 using UnityEngine;
-using System.Collections.Generic; // For using List<T>
 
-public class Enemy : MonoBehaviour
+public class Enemy : Interactable
 {
-    public Transform player;
-    public List<Enemy> enemiesInRange = new List<Enemy>(); // List to hold all enemies in range
-    public float enemyRadius = 1f;
-
-    public float health;
-
-    private int damage = 10;
-    // public int secondaryDamage = 5;
-
-    public KeyCode damageKey = KeyCode.Mouse0;
-    public KeyCode secondaryDamageKey = KeyCode.Mouse1;
 
     void Update()
     {
-        // Check for enemies every frame
-        CheckForEnemies();
-        
-        // You can add logic here to apply damage or interact with enemies in range
-        ApplyDamageToEnemies();
-        
+        CheckForInteractableWithInput();
     }
-
-    public void CheckForEnemies()
+    public override void Interact()
     {
-        // Get all colliders within the radius
-        Collider[] hitColliders = Physics.OverlapSphere(transform.position, enemyRadius);
-
-        // Clear the previous list of enemies in range
-        enemiesInRange.Clear();
-
-        // Loop through all the colliders that are within the radius
-        foreach (Collider col in hitColliders)
-        {
-            // Try to get the Enemy component from the collider
-            Enemy enemy = col.GetComponent<Enemy>();
-
-            // If the object is an enemy and it is within range
-            if (enemy != null && enemy.IsPlayerInRange(player))
-            {
-                // Add the enemy to the list
-                enemiesInRange.Add(enemy);
-            }
-       }
-
+        base.Interact();
+        // Attack enemy
     }
-
-    public bool IsPlayerInRange(Transform playerTransform)
-    {
-        // Check if the player is within the radius of this enemy
-        float distance = Vector3.Distance(playerTransform.position, transform.position);
-        return distance <= enemyRadius;
-    }
-
-    public virtual void ApplyDamageToEnemies()
-    {
-        // Example: enemy.TakeDamage(10);
-        if (Input.GetKeyDown(damageKey))
-        {
-            if (enemiesInRange.Count > 1 )
-            {
-                // If there are multiple enemies in range, apply damage to all of them
-
-                TakeDamage(damage);
-            }
-            
-            damage /= enemiesInRange.Count;
-            TakeDamage(damage);
-
-        }
-    }
-
-    // Optional: A method to handle damage to the enemy (you can customize this)
-    public virtual void TakeDamage(int _damage)
-    {
-        // Handle taking damage (e.g., reduce health, play a sound, etc.)
-        Debug.Log(name + " took " + _damage + " damage.");
-        
-        health -= _damage;
-
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        // Visualize the radius in the editor
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, enemyRadius);
-    }
-
-    
 }
